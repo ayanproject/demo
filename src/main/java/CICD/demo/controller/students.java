22184f52337b911561ca2566ea4daf2d8a9eee45
ayanproject <papaim294@gmail.com>
2026-02-08 19:11:36 +0530

    first commit

---
+++ src/main/java/CICD/demo/controller/students.java
@@ -0,0 +1,15 @@
+package CICD.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class students {
+    @GetMapping
+   public ResponseEntity<?>getStudent(){
+       return ResponseEntity.ok("This is all students :");
+    }
+}
